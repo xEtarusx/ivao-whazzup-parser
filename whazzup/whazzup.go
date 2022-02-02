9c563768667344b82c63eb64a6188642410f7b89
Gero Kindling <gero.kindling@web.de>
2022-02-02 15:20:47 +0100

    Add functions to get single server/voiceserver

--- whazzup/whazzup.go
+++ whazzup/whazzup.go
@@ -1,6 +1,7 @@
 package whazzup
 
 import (
+	"errors"
 	"github.com/xetarusx/ivao-whazzup-parser/client"
 	"github.com/xetarusx/ivao-whazzup-parser/connections"
 	"github.com/xetarusx/ivao-whazzup-parser/server"
@@ -19,4 +20,48 @@ type Whazzup struct {
 // GetConnectionsTotal Get total network connections
 func (w Whazzup) GetConnectionsTotal() int64 {
 	return w.Connections.Total
+}
+
+// GetServerIdList Get list of all server ids
+func (w Whazzup) GetServerIdList() []string {
+	var list []string
+
+	for _, s := range w.Servers {
+		list = append(list, s.Id)
+	}
+
+	return list
+}
+
+// GetServerById Return server struct for a specific id
+func (w Whazzup) GetServerById(id string) (server.Server, error) {
+	for _, s := range w.Servers {
+		if s.Id == id {
+			return s, nil
+		}
+	}
+
+	return server.Server{}, errors.New("server not found")
+}
+
+// GetVoiceServerIdList Get list of all voice server ids
+func (w Whazzup) GetVoiceServerIdList() []string {
+	var list []string
+
+	for _, s := range w.Servers {
+		list = append(list, s.Id)
+	}
+
+	return list
+}
+
+// GetVoiceServerById Return VoiceServer struct for a specific id
+func (w Whazzup) GetVoiceServerById(id string) (voiceServer.VoiceServer, error) {
+	for _, vs := range w.VoiceServers {
+		if vs.Id == id {
+			return vs, nil
+		}
+	}
+
+	return voiceServer.VoiceServer{}, errors.New("server not found")
 }
